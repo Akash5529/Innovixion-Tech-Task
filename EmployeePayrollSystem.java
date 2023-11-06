4129d08f4746b3beba1a84e8400814c219849193
Akash Patra <101459703+Akash5529@users.noreply.github.com>
2023-11-06 15:59:18 +0530

    Add files via upload

--- EmployeePayrollSystem.java
+++ EmployeePayrollSystem.java
@@ -1,114 +1,46 @@
 package Class_Object;
 
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class EmployeePayrollSystem {
 
-    private ArrayList<Employee> employees;
-
-    public EmployeePayrollSystem() {
-        this.employees = new ArrayList<>();
-    }
-
-    public void addEmployee(Employee employee) {
-        this.employees.add(employee);
-    }
-
-    public void calculatePayroll() {
-        for (Employee employee : this.employees) {
-            double salary = employee.getSalary();
-            double taxDeduction = calculateTaxDeduction(employee);
-            double netPay = salary - taxDeduction;
-
-            // Generate pay stub
-            PayStub payStub = new PayStub(employee, salary, taxDeduction, netPay);
-            employee.setPayStub(payStub);
-        }
-    }
-
-    private double calculateTaxDeduction(Employee employee) {
-
-        return employee.getSalary() * 0.1;
-    }
-
-    public void generatePayStubs() {
-        for (Employee employee : this.employees) {
-            PayStub payStub = employee.getPayStub();
-            System.out.println(payStub);
-        }
-    }
-
     public static void main(String[] args) {
-        EmployeePayrollSystem system = new EmployeePayrollSystem();
+        Scanner scanner = new Scanner(System.in);
 
-        // Add some employees
-        system.addEmployee(new Employee("John Doe", 50000));
-        system.addEmployee(new Employee("Jane Doe", 40000));
-        system.addEmployee(new Employee("John Doe", 80000));
-        system.addEmployee(new Employee("Jane Doe", 60000));
+        // Employee information
+        System.out.print("Enter employee name: ");
+        String name = scanner.nextLine();
 
-        // Calculate payroll
-        system.calculatePayroll();
+        System.out.print("Enter employee ID: ");
+        String id = scanner.nextLine();
 
-        // Generate pay stubs
-        system.generatePayStubs();
-    }
-}
+        System.out.print("Enter employee hourly rate: ");
+        double hourlyRate = scanner.nextDouble();
 
-class Employee {
-    private String name;
-    private double salary;
-    private PayStub payStub;
+        System.out.print("Enter number of hours worked: ");
+        int hoursWorked = scanner.nextInt();
 
-    public Employee(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+        // Salary calculations
+        double grossPay = hourlyRate * hoursWorked;
 
-    public String getName() {
-        return name;
-    }
+        // Tax deductions
+        double taxRate = 0.2; // Assuming a 20% tax rate
+        double taxDeduction = grossPay * taxRate;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+        // Net pay
+        double netPay = grossPay - taxDeduction;
 
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public PayStub getPayStub() {
-        return payStub;
-    }
-
-    public void setPayStub(PayStub payStub) {
-        this.payStub = payStub;
-    }
-}
-
-class PayStub {
-    private Employee employee;
-    private double salary;
-    private double taxDeduction;
-    private double netPay;
-
-    public PayStub(Employee employee, double salary, double taxDeduction, double netPay) {
-        this.employee = employee;
-        this.salary = salary;
-        this.taxDeduction = taxDeduction;
-        this.netPay = netPay;
-    }
-
-    @Override
-    public String toString() {
-        return "Pay Stub for " + employee.getName() + "\n" +
-                "Salary: $" + salary + "\n" +
-                "Tax Deduction: $" + taxDeduction + "\n" +
-                "Net Pay: $" + netPay;
+        // Generate pay stub
+        System.out.println("\nPay Stub");
+        System.out.println("-----------------------");
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Hourly Rate: $" + hourlyRate);
+        System.out.println("Hours Worked: " + hoursWorked);
+        System.out.println("Gross Pay: $" + grossPay);
+        System.out.println("Tax Deduction: $" + taxDeduction);
+        System.out.println("Net Pay: $" + netPay);
+        System.out.println("-----------------------");
     }
 }
